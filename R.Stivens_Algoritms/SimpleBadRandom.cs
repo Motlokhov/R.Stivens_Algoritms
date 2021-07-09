a06cde9cd3ca5a4eea64c05b8444d2c611f57c57
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-07-09 22:55:23 +0300

    Added simplebadrandom

---
+++ R.Stivens_Algoritms/SimpleBadRandom.cs
@@ -0,0 +1,18 @@
+﻿namespace R.Stivens_Algoritms
+{
+    public class SimpleBadRandom
+    {
+        private readonly int _from;
+        private readonly int _to;
+        public SimpleBadRandom(int from, int to)
+        {
+            _from = from;
+            _to = to;
+        }
+
+        public int Do(int value)
+        {
+            return _from + value % (_to - _from + 1);
+        }
+    }
+}
---
+++ Tests/SimpleRandomTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using R.Stivens_Algoritms;
+using System.Linq;
+
+namespace Tests
+{
+    [TestClass]
+    public class SimpleRandomTest
+    {
+        [TestMethod]
+        public void SimpleBadRandomTest()
+        {
+            SimpleBadRandom random = new(0, 1);
+            int[] values = new int[100];
+
+            for(int i = 0; i < 100; i++)
+                values[i] = random.Do(i);
+
+            //тут предсказуемость значений четные дают 0 нечетные 1
+            Assert.AreEqual(values.Count(_ => _ == 1),values.Count(_ => _ == 0));
+        }
+    }
+}
