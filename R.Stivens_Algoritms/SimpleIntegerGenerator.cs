b0eed732c8e607b1b8d3aafccc1055f850192d21
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-07-09 22:01:06 +0300

    Added simpleIntegerGenrator

---
+++ R.Stivens_Algoritms/SimpleIntegerGenerator.cs
@@ -0,0 +1,25 @@
+﻿namespace R.Stivens_Algoritms
+{
+    public class SimpleIntegerGenerator
+    {
+        private const int A = 7;
+        private const int B = 5;
+        private const int MOD = 11;
+        private int _last;
+
+        /// <summary>
+        /// Реализация простого ГПСЧ(линейный конгруэнтный генератор)[Повторяемый]
+        /// </summary>
+        /// <param name="from">Начальное значение</param>
+        public SimpleIntegerGenerator(int from)
+        {
+            _last = from; 
+        }
+
+        public int Do()
+        {
+            _last = (A * _last + B) % MOD;
+            return _last;
+        }
+    }
+}
---
+++ Tests/SimpleIntegerGenerator_Test.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using R.Stivens_Algoritms;
+
+namespace Tests
+{
+    [TestClass]
+    public class SimpleIntegerGenerator_Test
+    {
+        [TestMethod]
+        public void SimpleIntegerGeneratorRun()
+        {
+            SimpleIntegerGenerator random = new(0);
+            Assert.AreEqual(5, random.Do());
+            Assert.AreEqual(7, random.Do());
+            Assert.AreEqual(10, random.Do());
+            Assert.AreEqual(9, random.Do());
+            Assert.AreEqual(2, random.Do());
+            Assert.AreEqual(8, random.Do());
+            Assert.AreEqual(6, random.Do());
+            Assert.AreEqual(3, random.Do());
+            Assert.AreEqual(4, random.Do());
+            Assert.AreEqual(0, random.Do());
+        }
+    }
+}
