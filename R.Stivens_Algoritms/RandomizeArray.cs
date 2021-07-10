86c9c97be13837129b69571ce5478e5d5116a433
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-07-10 14:13:37 +0300

    Added randomize array
    +

---
+++ R.Stivens_Algoritms/RandomizeArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace R.Stivens_Algoritms
+{
+    /// <summary>
+    /// Равномерное распределение
+    /// </summary>
+    public class RandomizeArray
+    {
+        public static int[] Do(int[] array)
+        {
+            int maxIndex = array.Length;
+            int[] resultArray = new int[maxIndex];
+            array.CopyTo(resultArray, 0);
+            Random random = new();
+
+            for (int index = 0; index < maxIndex; index++)
+            {
+                int randomIndex = random.Next(0, maxIndex);
+                int value = array[index];
+                int valueFromRandomIndex = array[randomIndex];
+                array[randomIndex] = value;
+                array[index] = valueFromRandomIndex;
+            }
+            return array;
+        }
+    }
+}
--- Tests/SimpleRandomTest.cs
+++ Tests/SimpleRandomTest.cs
@@ -19,5 +19,11 @@ namespace Tests
             //тут предсказуемость значений четные дают 0 нечетные 1
             Assert.AreEqual(values.Count(_ => _ == 1),values.Count(_ => _ == 0));
         }
+
+        [TestMethod]
+        public void RandomArrayTest()
+        {
+            int[] newArray = RandomizeArray.Do(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 });
+        }
     }
 }
