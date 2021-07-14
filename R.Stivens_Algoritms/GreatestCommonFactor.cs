feeebefd8004d340d5329d2ad71f3883ff5de1f6
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-07-14 13:34:35 +0300

    Addede GCF algorithm

---
+++ R.Stivens_Algoritms/GreatestCommonFactor.cs
@@ -0,0 +1,17 @@
+﻿namespace R.Stivens_Algoritms
+{
+    //Наибольший общий делитель (алгоритм Евклида)
+    public static class GreatestCommonFactor
+    {
+        public static int Do(int first, int second)
+        {
+            while(second != 0)
+            {
+                int remainder = first % second;
+                first = second;
+                second = remainder;
+            }
+            return first;
+        }
+    }
+}
---
+++ Tests/IntegerAlgorithm_Test.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using R.Stivens_Algoritms;
+
+namespace Tests
+{
+    [TestClass]
+    public class IntegerAlgorithm_Test
+    {
+        [TestMethod]
+        public void GCF_Test()
+        {
+            Assert.AreEqual(GreatestCommonFactor.Do(4851, 3003), 231);
+        }
+
+        [TestMethod]
+        public void GCF_Test_Invert()
+        {
+            Assert.AreEqual(GreatestCommonFactor.Do(3003, 4851), 231);
+        }
+    }
+}
