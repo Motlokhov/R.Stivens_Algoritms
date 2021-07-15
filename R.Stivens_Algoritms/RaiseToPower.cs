f3fce0db6cb7fde1dd0a7879912c2d59d1223582
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-07-15 19:41:46 +0300

    Added RaiseToPowerAlgorithm

---
+++ R.Stivens_Algoritms/RaiseToPower.cs
@@ -0,0 +1,49 @@
+﻿namespace R.Stivens_Algoritms
+{
+    public static class RaiseToPower
+    {
+        public static int Do(int A, int P)
+        {
+            int result = 1;
+            int binaryPower = A;
+            string binary = ToBinary(P).ToString();
+            for(int index = binary.Length-1; index >=0; index--)
+            {
+                if(binary[index] == '1')
+                    result *= binaryPower;;
+
+                binaryPower *= binaryPower;
+            }
+
+            return result;
+        }
+
+        private static int ToBinary(int A)
+        {
+            if(A <= 1)
+                return A;
+
+            const int binaryModifier = 2;
+            const int decimalModifier = 10;
+            int length = 0;
+            int N = 1;
+
+            do
+            {
+                N *= 2;
+                length++;
+            } while(N <= A);
+
+            int innerA = A;
+            int result = 0;
+            for(int i = 0; i < length; i++)
+            {
+                result += innerA % binaryModifier * Do(decimalModifier, i);
+                innerA /= binaryModifier;
+            }
+
+            result += innerA * Do(decimalModifier, length - 1);
+            return result;
+        }
+    }
+}
--- Tests/IntegerAlgorithm_Test.cs
+++ Tests/IntegerAlgorithm_Test.cs
@@ -17,5 +17,35 @@ namespace Tests
         {
             Assert.AreEqual(GreatestCommonFactor.Do(3003, 4851), 231);
         }
+
+        [TestMethod]
+        public void Raise_7_ToPower_7_AreEqual_823_543()
+        {
+            Assert.AreEqual(RaiseToPower.Do(7, 7), 823_543);
+        }
+
+        [TestMethod]
+        public void Raise_7_ToPower_8_AreEqual_5_764_801()
+        {
+            Assert.AreEqual(RaiseToPower.Do(7, 8), 5_764_801);
+        }
+
+        [TestMethod]
+        public void Raise_Negative_7_ToPower_7_AreEqual_Negative_823_543()
+        {
+            Assert.AreEqual(RaiseToPower.Do(-7, 7), -823_543);
+        }
+
+        [TestMethod]
+        public void Raise_0_ToPower_0_AreEqual_1()
+        {
+            Assert.AreEqual(RaiseToPower.Do(0, 0),1);
+        }
+
+        [TestMethod]
+        public void Raise_0_ToAnyPowerGreaterThen_0_AreEqualZero()
+        {
+            Assert.AreEqual(RaiseToPower.Do(0, 7), 0);
+        }
     }
 }
