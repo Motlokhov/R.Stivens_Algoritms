76bd43762569eba151958e91a3f7cb34ea839edc
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-07-25 20:53:16 +0300

    Added find factors

---
+++ R.Stivens_Algoritms/FindFactors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace R.Stivens_Algoritms
+{
+    public static class FindFactors
+    {
+        public static int[] Do(int number)
+        {
+            List<int> factors = new();
+            for(int factor = 2; factor <= (int)Math.Sqrt(number); factor++)
+            {
+                while(number % factor == 0)
+                {
+                    number /= factor;
+                    factors.Add(factor);
+                }
+            }
+            if(number > 1)
+                factors.Add(number);
+            return factors.ToArray();
+        }
+    }
+}
---
+++ Tests/FindFactors_Test.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using R.Stivens_Algoritms;
+
+namespace Tests
+{
+    [TestClass]
+    public class FindFactors_Test
+    {
+        [TestMethod]
+        public void FindFactors144()
+        {
+            CollectionAssert.AreEqual(new int[] { 2, 2, 2, 2, 3, 3 }, FindFactors.Do(144));
+        }
+
+        [TestMethod]
+        public void FindFactors128()
+        {
+            CollectionAssert.AreEqual(new int[] { 2, 2, 2, 2, 2, 2, 2 }, FindFactors.Do(128));
+        }
+
+        [TestMethod]
+        public void FindFactors127()
+        {
+            CollectionAssert.AreEqual(new int[] { 127 }, FindFactors.Do(127));
+        }
+    }
+}
