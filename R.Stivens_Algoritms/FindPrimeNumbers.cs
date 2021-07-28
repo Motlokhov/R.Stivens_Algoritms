3291aaad64b64a2aa2be8fc45fe229a37a6be0f3
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-07-28 19:11:18 +0300

    Added FindPrimeNumbers

---
+++ R.Stivens_Algoritms/FindPrimeNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace R.Stivens_Algoritms
+{
+    public static class FindPrimeNumbers
+    {
+        public static int[] Do(int maxNumber)
+        {
+            bool[] isComplex = new bool[maxNumber + 1];
+
+            for(int factor = 4; factor <= maxNumber; factor += 2)
+                isComplex[factor] = true;
+
+            // Первых шаг берем квадрат модификатора, так как все числа до квадарата модификатора уже помечены
+            // каждый следующий шаг берем сумму двух модификаторов так как большинство уже закрыто. (редко встречаются уже помеченные)
+            // возможно можно вычислить зависимость которая не будет помечать уже помеченные числа
+            for(int factor = 3; factor <= Math.Sqrt(maxNumber); factor += 2)
+                for(int number = factor * factor; number <= maxNumber; number += factor * 2)
+                    isComplex[number] = true;
+
+            var list = new List<int>(maxNumber / 4);
+
+            for(int number = 2; number <= maxNumber; number++)
+                if(isComplex[number] is false)
+                    list.Add(number);
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ Tests/FindPrime_Test.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using R.Stivens_Algoritms;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class FindPrime_Test
+    {
+        [TestMethod]
+        public void FindPrimesOf_0_and_1()
+        {
+            int[] primes = Array.Empty<int>();
+
+            CollectionAssert.AreEqual(primes, FindPrimeNumbers.Do(0));
+            CollectionAssert.AreEqual(primes, FindPrimeNumbers.Do(1));
+        }
+
+        [TestMethod]
+        public void FindPrimesOf199()
+        {
+            int[] primes = new int[] 
+            {
+                2,3,5,7,11,13,17,19,
+                23,29,31,37,41,43,47,
+                53,59,61,67,71,73,79,
+                83,89,97,101,103,107,109,
+                113,127,131,137,139,149,
+                151,157,163,167,173,179,
+                181,191,193,197,199
+            };
+
+            CollectionAssert.AreEqual(primes, FindPrimeNumbers.Do(199));
+        }
+    }
+}
