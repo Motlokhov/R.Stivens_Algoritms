fa0e3cbaedd7a8292b2a702ef8d6e19dab65460a
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-08-08 22:34:50 +0300

    Added OneWayLinkedList

---
+++ R.Stivens_Algoritms/OneWayLinkedList.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace R.Stivens_Algoritms
+{
+    public class OneWayLinkedList
+    {
+        // Note: в реализации от Microsoft linked list имеет закольцованую структуру
+        private OneWayLinkedListNode first;
+
+        public OneWayLinkedList()
+        {
+            first = new OneWayLinkedListNode();
+        }
+
+        public class OneWayLinkedListNode
+        {
+            public int Value { get; set; }
+            public OneWayLinkedListNode? Next { get; internal set; }
+        }
+
+        public void ActionIterate (Action<int> action)
+        {
+            OneWayLinkedListNode current = first;
+            while(current != null)
+            {
+                action(current.Value);
+                current = current.Next;
+            }
+        }
+
+        public OneWayLinkedListNode Find(int target)
+        {
+            OneWayLinkedListNode current = first;
+            while(current != null)
+            {
+                if(current.Value == target)
+                    return current;
+                current = current.Next;
+            }
+            return null;
+        }
+
+        public OneWayLinkedListNode FindNodeBefore(int value)
+        {
+            OneWayLinkedListNode current = first;
+            while(current.Next != null)
+            {
+                if(current.Next.Value == value)
+                    return current;
+                current = current.Next;
+            }
+
+            return null;
+        }
+
+        public void AddAtTop(int value)
+        {
+            OneWayLinkedListNode @new = new()
+            {
+                Value = value,
+                Next = first
+            };
+            
+            first = @new;
+        }
+
+        public void AddAtEnd(int value)
+        {
+            OneWayLinkedListNode current = first;
+            while(current.Next != null)
+                current = current.Next;
+
+            current.Next = new OneWayLinkedListNode { Value = value };
+        }
+
+        public void AddAfter(int value, int newValue)
+        {
+            OneWayLinkedListNode node = Find(value);
+            if(node == null)
+                throw new Exception("Node not found");
+
+            OneWayLinkedListNode nodeAfter = node.Next;
+            node.Next = new OneWayLinkedListNode { Value = newValue };
+            node.Next.Next = nodeAfter;
+        }
+    }
+
+    
+}
---
+++ Tests/OneWayLinkedList_Test.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using R.Stivens_Algoritms;
+using System;
+using System.Diagnostics;
+using static R.Stivens_Algoritms.OneWayLinkedList;
+
+namespace Tests
+{
+    [TestClass]
+    public class OneWayLinkedList_Test
+    {
+        readonly OneWayLinkedList sut = new();
+
+        public OneWayLinkedList_Test()
+        {
+            sut.AddAtTop(2);
+            sut.AddAtTop(1);
+        }
+
+        [TestMethod]
+        public void FindItemsTest()
+        {
+            Assert.AreEqual(1, sut.Find(1).Value);
+            Assert.AreEqual(0, sut.Find(0).Value);
+            Assert.IsNull(sut.Find(-1));
+        }
+
+        [TestMethod]
+        public void ActionIterateTest()
+        {
+            sut.ActionIterate((value) => Trace.Write($"Value:{value} {Environment.NewLine}"));
+        }
+
+        [TestMethod]
+        public void AddAtEndTest()
+        {
+            sut.AddAtEnd(3);
+            OneWayLinkedListNode node = sut.Find(0);
+
+            Assert.AreEqual(3, node.Next.Value);
+        }
+
+        [TestMethod]
+        public void AddAfterTest()
+        {
+            sut.AddAfter(0,3);
+            OneWayLinkedListNode node = sut.Find(0);
+
+            Assert.AreEqual(3, node.Next.Value);
+        }
+    }
+}
