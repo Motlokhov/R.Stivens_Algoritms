b4974937e359976ed8106536796ea270b0bd2106
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-08-11 16:29:28 +0300

    For IsPrime alg. added info about new realization

--- R.Stivens_Algoritms/IsPrime.cs
+++ R.Stivens_Algoritms/IsPrime.cs
@@ -5,6 +5,7 @@ namespace R.Stivens_Algoritms
     public static class IsPrime
     {
         //Посмотреть в сторону реализаций https://habr.com/ru/post/526924/
+        //Проверка на простоту опубликованная в 2002 году https://www.youtube.com/watch?v=ASIbwazJFhQ
         public static bool Do(int p)
         {
             if(p % 2 == 0)
