a802644057d5161c9ca33ad06daf38aae30551e0
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-08-17 19:45:27 +0300

    Realized as wheel structure (linked list) p.1

--- R.Stivens_Algoritms/LinkedList.cs
+++ R.Stivens_Algoritms/LinkedList.cs
@@ -2,99 +2,92 @@
 
 namespace R.Stivens_Algoritms
 {
-    //Реализация с ограничителями по краям(в данной реализации невозможно грамотно сделать Count)
+    //Реализация с через кольцевую структуру.
     public class LinkedList
     {
-        // Note: в реализации от Microsoft linked list имеет закольцованую структуру
         private LinkedListNode head;
-        private LinkedListNode tail;
-
-        public LinkedList()
-        {
-            head = new ();
-            tail = new ();
-
-            tail.Preveous = head;
-            head.Next = tail;
-        }
 
         public class LinkedListNode
         {
-            public LinkedListNode(int value = default)
+            internal int _value;
+            internal LinkedListNode _next;
+            internal LinkedListNode _previous;
+            internal LinkedList _list;
+
+            public LinkedListNode(int value)
             {
-                Value = value;
+                _value = value;
             }
 
-            public int Value { get; set; }
-            public LinkedListNode Next { get; internal set; }
-            public LinkedListNode Preveous { get; internal set; }
-        }
-
-        public static void ActionIterate (Action<int> action, LinkedListNode startNode)
-        {
-            LinkedListNode current = startNode;
-            while(current != null)
-            {
-                action(current.Value);
-                current = current.Next;
-            }
+            public int Value { get => _value;}
+            public LinkedListNode Next => _next == _list.head ? null : _next;
+            public LinkedListNode Previous => _list.head == this ? null : _previous;
+            internal LinkedList List => _list;
         }
 
         public LinkedListNode Find(int target)
         {
             LinkedListNode current = head;
-            while(current != null)
+
+            do
             {
                 if(current.Value == target)
                     return current;
-                current = current.Next;
-            }
+                current = current._next;
+            } while(current != head);
+
             return null;
         }
 
         public LinkedListNode FindFrom(LinkedListNode node, int target)
         {
-            LinkedListNode current = node.Next;
-            while(current != null)
+            LinkedListNode current = node;
+
+            do
             {
                 if(current.Value == target)
                     return current;
-                current = current.Next;
-            }
+                current = current._next;
+            } while(current != head);
+
             return null;
         }
 
-        public LinkedListNode FindNodeBefore(int value)
+        public LinkedListNode FindBefore(int value)
         {
             LinkedListNode current = head;
-            while(current.Next != null)
+
+            do
             {
-                if(current.Next.Value == value)
+                if(current._next.Value == value)
                     return current;
-                current = current.Next;
-            }
+                current = current._next;
+            } while(current._next != head);
 
             return null;
         }
 
-        public void AddAtTop(int value)
-        {
-            LinkedListNode @new = new()
-            {
-                Value = value,
-                Next = head
-            };
-
-            head.Preveous = @new;
-            head = @new;
-        }
-
-        public void AddAtEnd(int value)
+        public void AddFirst(int value)
         {
             LinkedListNode newNode = new(value);
-            newNode.Preveous = tail;
-            tail.Next= newNode;
-            tail = newNode;
+            if(head == null)
+                InsertNodeToEmptyList(newNode);
+            else
+            {
+                InsertNodeBefore(head, newNode);
+                head = newNode;
+            }
+            newNode._list = this;
+        }
+
+        public void AddLast(int value)
+        {
+            LinkedListNode newNode = new(value);
+            if(head == null)
+                InsertNodeToEmptyList(newNode);
+            else
+                InsertNodeBefore(head, newNode);
+            newNode._list = this;
         }
 
         public void AddAfter(int value, int newValue)
@@ -103,17 +96,14 @@ namespace R.Stivens_Algoritms
             if(node == null)
                 throw new Exception("Node not found");
 
-            AddAfter(node, newValue);
+            LinkedListNode newNode = new(newValue);
+            AddAfter(node, newNode);
         }
 
-        public void AddAfter(LinkedListNode arterMe, int newValue)
+        public void AddAfter(LinkedListNode node, LinkedListNode newNode)
         {
-            LinkedListNode newNode = new(newValue);
-            newNode.Next = arterMe.Next;
-            newNode.Preveous = arterMe;
-
-            arterMe.Next.Preveous = newNode;
-            arterMe.Next = newNode;
+            InsertNodeBefore(node._next, newNode);
+            newNode._list = this;
         }
 
         public void AddBefore(int value, int newValue)
@@ -122,17 +112,32 @@ namespace R.Stivens_Algoritms
             if(node == null)
                 throw new Exception("Node not found");
 
-            AddBefore(node, newValue);
+            LinkedListNode newNode = new(newValue);
+            AddBefore(node, newNode);
         }
 
-        private void AddBefore(LinkedListNode beforeMe, int newValue)
+        public void AddBefore(LinkedListNode node, LinkedListNode newNode)
         {
-            LinkedListNode newNode = new(newValue);
-            newNode.Next = beforeMe;
-            newNode.Preveous = beforeMe.Preveous;
+            InsertNodeBefore(node,newNode);
+            if(node == head)
+                head = newNode;
+            newNode._list = this;
+        }
 
-            beforeMe.Preveous.Next = newNode;
-            beforeMe.Preveous = newNode;
+        private void InsertNodeToEmptyList(LinkedListNode node)
+        {
+            node._previous = node;
+            node._next = node;
+            head = node;
+        }
+
+        private void InsertNodeBefore(LinkedListNode node, LinkedListNode newNode)
+        {
+            newNode._next = node;
+            newNode._previous = node._previous;
+
+            node._previous._next = newNode;
+            node._previous = newNode;
         }
     }
 }
