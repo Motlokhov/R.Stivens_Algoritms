cebadb55534dc54e67328bd915046de186ec4392
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-08-28 00:05:48 +0300

    Added HasLoopUsingHashTable

---
+++ R.Stivens_Algoritms/LinkedNode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+namespace R.Stivens_Algoritms
+{
+    public class LinkedNode
+    {
+        
+        public LinkedNode Next { get; set; }
+
+        public static bool HasLoopUsingHashTable(LinkedNode start)
+        {
+            Hashtable visited = new();
+
+            LinkedNode node = start;
+            while(node is not null)
+            {
+                if(visited.Contains(node))
+                    return true;
+                visited.Add(node, node);
+                node = node.Next;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Tests/InfinityLoop_Tests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using R.Stivens_Algoritms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class InfinityLoop_Tests
+    {
+        readonly LinkedNode loopNode;
+        readonly LinkedNode unloopNode;
+
+        public InfinityLoop_Tests()
+        {
+            loopNode = new LinkedNode
+            {
+                Next = new LinkedNode()
+            };
+
+            loopNode.Next.Next = loopNode;
+
+            unloopNode = new LinkedNode()
+            {
+                Next = new LinkedNode()
+            };
+        }
+
+        [TestMethod]
+        public void IsLooped_Test()
+        {
+            Assert.IsTrue(LinkedNode.HasLoopUsingHashTable(loopNode));
+            Assert.IsFalse(LinkedNode.HasLoopUsingHashTable(unloopNode));
+        }
+    }
+}
