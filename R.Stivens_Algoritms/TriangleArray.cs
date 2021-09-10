6207cb8c7fdfa8fbe24d10bdc359a7af5fbdf4f6
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-09-10 21:21:21 +0300

    Added TriangleArray

---
+++ R.Stivens_Algoritms/TriangleArray.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R.Stivens_Algoritms
+{
+    class TriangleArray
+    {
+        int[][] _array;
+
+        public TriangleArray(int n = 1)
+        {
+            _array = new int[n][];
+
+            for(int i = 0; i < n; i++)
+                _array[i] = new int[i + 1];
+        }
+
+        public int this[int row, int col]
+        {
+            get 
+            {
+                Rule(ref row, ref col);
+                return _array[row][col]; 
+            }
+
+            set
+            {
+                Rule(ref row, ref col);
+                _array[row][col] = value;
+            }
+        }
+
+        private void Rule(ref int row, ref int col)
+        {
+            if(row >= _array.Length)
+                throw new ArgumentException("");
+
+            if(col > row)
+            {
+                int temp = row;
+                row = col;
+                col = temp;
+            }
+        }
+    }
+}
---
+++ Tests/Array_Test.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using R.Stivens_Algoritms;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class Array_Test
+    {
+        [TestMethod]
+        public void TriangleArrayTest()
+        {
+            TriangleArray triangleArray = new(3);
+            triangleArray[0, 0] = 100;
+            triangleArray[1, 1] = 102;
+            triangleArray[2, 2] = 104;
+
+            Assert.AreEqual(100, triangleArray[0, 0]);
+            Assert.AreEqual(102, triangleArray[1, 1]);
+            Assert.AreEqual(104, triangleArray[2, 2]);
+
+            triangleArray[1, 0] = 302;
+            Assert.AreEqual(302, triangleArray[0, 1]);
+
+            triangleArray[2, 0] = 303;
+            Assert.AreEqual(303, triangleArray[0, 2]);
+
+            Assert.ThrowsException<ArgumentException>(() => triangleArray[3, 3]);
+        }
+    }
+}
