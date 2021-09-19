47c82d4b6d7b62f51a227090689ced0cc4e69cc5
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-09-19 20:09:54 +0300

    Added doublestack

---
+++ R.Stivens_Algoritms/DoubleStack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R.Stivens_Algoritms
+{
+    public class DoubleStack
+    {
+        private string[] _data;
+        private int _leftIndex, _rightIndex;
+        private int leftBorder { get => -1; }
+        private int rightBorder { get => _data.Length; }
+        public DoubleStack(int length)
+        {
+            _data = new string[length];
+            _leftIndex = leftBorder;
+            _rightIndex = rightBorder;
+        }
+
+        public void PushLeft(string value)
+        {
+            CheckIndexesForIntersection();
+            _data[++_leftIndex] = value;
+        }
+
+        public string PopLeft()
+        {
+            if(_leftIndex == leftBorder)
+                throw new ArgumentException();
+            return _data[_leftIndex--];
+        }
+
+        public void PushRight(string value)
+        {
+            CheckIndexesForIntersection();
+            _data[--_rightIndex] = value;
+        }
+
+        public string PopRight()
+        {
+            if(_rightIndex == rightBorder)
+                throw new ArgumentException();
+            return _data[_rightIndex++];
+        }
+
+        private void CheckIndexesForIntersection()
+        {
+            if(_rightIndex - _leftIndex == 1)
+                throw new ArgumentException();
+        }
+    }
+}
---
+++ Tests/Stack_Test.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using R.Stivens_Algoritms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class Stack_Test
+    {
+        [TestMethod]
+        public void DoubleStack_Test()
+        {
+            DoubleStack doubleStack = new(3);
+            doubleStack.PushLeft("left 1");
+            doubleStack.PushLeft("left 2");
+            doubleStack.PushLeft("middle");
+
+            Assert.ThrowsException<ArgumentException>(() => { doubleStack.PushLeft("exception"); });
+
+            Assert.AreEqual("middle", doubleStack.PopLeft());
+            Assert.AreEqual("left 2", doubleStack.PopLeft());
+            Assert.AreEqual("left 1", doubleStack.PopLeft());
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                Assert.AreEqual("exception", doubleStack.PopLeft());
+            });
+
+            doubleStack.PushRight("right 1");
+            doubleStack.PushRight("right 2");
+            doubleStack.PushRight("middle");
+
+            Assert.ThrowsException<ArgumentException>(() => { doubleStack.PushLeft("exception"); });
+
+            Assert.AreEqual("middle", doubleStack.PopRight());
+            Assert.AreEqual("right 2", doubleStack.PopRight());
+            Assert.AreEqual("right 1", doubleStack.PopRight());
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                Assert.AreEqual("exception", doubleStack.PopRight());
+            });
+        }
+    }
+}
