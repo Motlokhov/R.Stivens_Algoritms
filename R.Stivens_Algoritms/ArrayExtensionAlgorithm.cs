8d61967d539edb4cacdbe9e59451375939b32753
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-12-11 20:55:48 +0300

    Added InsertionArraySort

---
+++ R.Stivens_Algoritms/ArrayExtensionAlgorithm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace R.Stivens_Algoritms
+{
+    public static class ArrayExtensionAlgorithm
+    {
+        private static void Swap(ref int left,ref int right)
+        {
+            int temp = left;
+            left = right;
+            right = temp;
+        }
+
+        public static void InsertionSort(this int[] array, bool asc = true)
+        {
+            if (array == null || array.Length == 0)
+            {
+                return;
+            }
+
+            static bool ascSort(int @new, int sorted) => @new < sorted;
+            static bool descSort(int @new, int sorted) => @new > sorted;
+            Func<int, int, bool> sort = asc ? ascSort : descSort;
+
+            for (int index = 0; index < array.Length; index++)
+            {
+                for (int sortedIndex = index - 1; sortedIndex >= 0; sortedIndex--)
+                {
+                    int newIndex = sortedIndex + 1;
+                    if (sort(array[newIndex], array[sortedIndex]))
+                    {
+                        Swap(ref array[newIndex], ref array[sortedIndex]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Tests/ArraySort_Test.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using R.Stivens_Algoritms;
+
+namespace Tests
+{
+    [TestClass]
+    public class ArraySort_Test
+    {
+        [TestMethod]
+        public void InsertionSortAscTest()
+        {
+            int[] array = new[] { 10, 2, 1, 5, 3, 4, 1, 3 };
+            array.InsertionSort();
+
+            CollectionAssert.AreEqual(new[] { 1, 1, 2, 3, 3, 4, 5, 10 }, array);
+        }
+
+        [TestMethod]
+        public void InsertionSortDescTest()
+        {
+            int[] array = new[] { 10, 2, 1, 5, 3, 4, 1, 3 };
+            array.InsertionSort(false);
+
+            CollectionAssert.AreEqual(new[] { 10, 5, 4, 3, 3, 2, 1, 1 }, array);
+        }
+    }
+}
