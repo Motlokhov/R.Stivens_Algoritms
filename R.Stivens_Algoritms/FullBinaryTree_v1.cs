9f23b3bd098dcef168ed6c82e83bfc75147ca5e5
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2022-01-06 19:26:34 +0300

    Added FullbinaryTree_v1 Add Realisation

---
+++ R.Stivens_Algoritms/FullBinaryTree_v1.cs
@@ -0,0 +1,65 @@
+﻿namespace R.Stivens_Algoritms
+{
+    internal class FullBinaryTree_v1
+    {
+        public BinaryNode Root { get; private set; }
+
+        public void Add(long value)
+        {
+            if (Root == null)
+            {
+                Root = new BinaryNode(null, value);
+                return;
+            }
+
+            AddNode(Root, value);
+        }
+
+        private void AddNode(BinaryNode root, long value)
+        {
+            AddToLayer(value, root);
+        }
+
+        private void AddToLayer(long value,params BinaryNode[] nodes)
+        {
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                if (nodes[i].Left == null)
+                {
+                    nodes[i].Left = new BinaryNode(nodes[i], value);
+                    return;
+                }
+                else if(nodes[i].Right == null)
+                {
+                    nodes[i].Right = new BinaryNode(nodes[i], value);
+                    return;
+                }
+            }
+
+            BinaryNode[] children = new BinaryNode[nodes.Length * BinaryNode.Binary];
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                children[i * BinaryNode.Binary] = nodes[i].Left;
+                children[i * BinaryNode.Binary + 1] = nodes[i].Right;
+            }
+
+            AddToLayer(value, children);
+        }
+
+        public class BinaryNode
+        {
+            public const byte Binary = 2;
+
+            public BinaryNode(BinaryNode parent, long value)
+            {
+                Parent = parent;
+                Value = value;
+            }
+
+            public BinaryNode Parent { get; internal set; }
+            public BinaryNode Left { get; internal set; }
+            public BinaryNode Right { get; internal set; }
+            public long Value { get; init; }
+        }
+    }
+}
---
+++ Tests/FullBinaryTree_Test.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using R.Stivens_Algoritms;
+
+namespace Tests
+{
+    [TestClass]
+    public class FullBinaryTree_Test
+    {
+        [TestMethod]
+        public void AddValues_Test()
+        {
+            FullBinaryTree_v1 tree = new();
+            tree.Add(0);
+            tree.Add(1);
+            tree.Add(2);
+            tree.Add(3);
+            tree.Add(4);
+            tree.Add(5);
+            tree.Add(6);
+            tree.Add(7);
+            tree.Add(8);
+
+            Assert.AreEqual(0, tree.Root.Value);
+            Assert.AreEqual(1, tree.Root.Left.Value);
+            Assert.AreEqual(2, tree.Root.Right.Value);
+            Assert.AreEqual(3, tree.Root.Left.Left.Value);
+            Assert.AreEqual(4, tree.Root.Left.Right.Value);
+            Assert.AreEqual(5, tree.Root.Right.Left.Value);
+            Assert.AreEqual(6, tree.Root.Right.Right.Value);
+            Assert.AreEqual(7, tree.Root.Left.Left.Left.Value);
+            Assert.AreEqual(8, tree.Root.Left.Left.Right.Value);
+
+            Assert.AreEqual(tree.Root, tree.Root.Left.Parent);
+            Assert.AreEqual(tree.Root, tree.Root.Right.Parent);
+            Assert.AreEqual(tree.Root.Left, tree.Root.Left.Left.Parent);
+            Assert.AreEqual(tree.Root.Left, tree.Root.Left.Right.Parent);
+            Assert.AreEqual(tree.Root.Right, tree.Root.Right.Left.Parent);
+            Assert.AreEqual(tree.Root.Right, tree.Root.Right.Right.Parent);
+        }
+    }
+}
