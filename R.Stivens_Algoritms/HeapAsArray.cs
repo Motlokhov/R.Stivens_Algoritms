116d97cde3e3cab89f2b8dbe342eeead89077907
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2022-02-05 22:06:57 +0300

    Added SortArrayToHeapStructure

---
+++ R.Stivens_Algoritms/HeapAsArray.cs
@@ -0,0 +1,38 @@
+﻿namespace R.Stivens_Algoritms
+{
+    internal class HeapAsArray
+    {
+        /// <summary>
+        /// Сортирует массив представляя в виде структуры куча(heap), где каждый дочерний элемент меньше или равен родительскому.
+        /// </summary>
+        /// <param name="array"></param>
+        public static void SortArrayToHeapStructure(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int currentIndex = i;
+                while (currentIndex != 0)
+                {
+                    int parentIndex = (currentIndex - 1) / 2;
+                    int currentValue = array[currentIndex];
+                    int parrentValue = array[parentIndex];
+
+                    if (currentValue <= parrentValue)
+                    {
+                        break;
+                    }
+
+                    Swap(ref array[currentIndex],ref array[parentIndex]);
+                    currentIndex = parentIndex;
+                }
+            }
+        }
+
+        private static void Swap(ref int left, ref int right)
+        {
+            int temp = left;
+            left = right;
+            right = temp;
+        }
+    }
+}
---
+++ Tests/MakeHeap_Test.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using R.Stivens_Algoritms;
+using System.Linq;
+
+namespace Tests
+{
+    [TestClass]
+    public class MakeHeap_Test
+    {
+        [TestMethod]
+        public void MakeHeapFromArrayTest()
+        {
+            int[] array = { 0, 1, 2, 3, 4, 5, 6 ,7,8,9,10,11,12,13,14 };
+            HeapAsArray.SortArrayToHeapStructure(array);
+
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                int parrentIndex = (i - 1) / 2;
+                if (parrentIndex > 0)
+                {
+                    Assert.IsTrue(array[parrentIndex] >= array[i]);
+                }
+            }
+        }
+    }
+}
