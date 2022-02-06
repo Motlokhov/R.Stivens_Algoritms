2585170252f3b2aa2ae2db8dea3bed6b173a4bc8
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2022-02-06 20:49:35 +0300

    Added RemoveTop from heap

--- R.Stivens_Algoritms/HeapAsArray.cs
+++ R.Stivens_Algoritms/HeapAsArray.cs
@@ -1,4 +1,6 @@
-﻿namespace R.Stivens_Algoritms
+﻿using System;
+
+namespace R.Stivens_Algoritms
 {
     internal class HeapAsArray
     {
@@ -25,6 +27,48 @@
                     Swap(ref array[currentIndex],ref array[parentIndex]);
                     currentIndex = parentIndex;
                 }
+            }
+        }
+
+        public static void RemoveTop(int[] array)
+        {
+            int result = array[0];
+            array[0] = -1;
+
+            int index = 0;
+            while (true)
+            {
+                int childIndex1 = index * 2 + 1;
+                int childIndex2 = index * 2 + 2;
+
+                if (childIndex1 > array.Length)
+                {
+                    childIndex1 = index;
+                    childIndex2 = index;
+                }
+                else if(childIndex2 > array.Length)
+                {
+                    childIndex2 = index;
+                }
+
+
+                if (array[childIndex1] <= array[index] && array[childIndex2] <= array[index])
+                {
+                    break;
+                }
+
+                int swapIndex;
+                if (array[childIndex1] > array[childIndex2])
+                {
+                    swapIndex = childIndex1;
+                }
+                else
+                {
+                    swapIndex = childIndex2;
+                }
+
+                Swap(ref array[index],ref array[swapIndex]);
+                index = swapIndex;
             }
         }
 
--- Tests/MakeHeap_Test.cs
+++ Tests/MakeHeap_Test.cs
@@ -11,7 +11,7 @@ namespace Tests
         [TestMethod]
         public void MakeHeapFromArrayTest()
         {
-            int[] array = { 0, 1, 2, 3, 4, 5, 6 ,7,8,9,10,11,12,13,14 };
+            int[] array = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
             HeapAsArray.SortArrayToHeapStructure(array);
 
             for (int i = array.Length - 1; i > 0; i--)
@@ -22,6 +22,15 @@ namespace Tests
                     Assert.IsTrue(array[parrentIndex] >= array[i]);
                 }
             }
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 5, 3, 1, 3, 2 }, new int[] { 3, 3, 1, -1, 2 })]
+        [DataRow(new int[] { 14, 9, 13, 6, 8, 10, 12, 0, 3, 2, 7, 1, 5, 4, 11 }, new int[] { 13, 9, 12, 6, 8, 10, 11, 0, 3, 2, 7, 1, 5, 4, -1 })]
+        public void RemoveTopHeapTest(int[] array, int[] expected)
+        {
+            HeapAsArray.RemoveTop(array);
+            CollectionAssert.AreEqual(array, expected);
         }
     }
 }
